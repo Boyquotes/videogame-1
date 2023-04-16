87dbcdd42f95c2af725893b36db1266e2649fc46
Oscar Mellado <oscar-mellado@uoc.edu>
2023-04-17 01:53:30 +0200

    Add intro timeout

--- Main.gd
+++ Main.gd
@@ -5,19 +5,30 @@ extends Node2D
 @onready var background: Node2D = $SpaceBackground/SpaceBackground
 @onready var background2: Node2D = $SpaceBackground/SpaceBackground2
 @onready var death_area: Area2D = $DeathArea
+@onready var ui_info := $CanvasLayer/Info
+@onready var timer := $Timer
 
 func _ready():
+	set_all_processes(false)
 	death_area.body_exited.connect(_on_exited_death_area)
-
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+	print(timer.time_left)
 
 func _process(delta):
 	pass
+	
+func start_game() -> void:
+	set_all_processes(true)
 
 func finish_game() -> void:
 	set_all_processes(false)
-
+	
 func _on_exited_death_area(body: Node) -> void:
 	finish_game()
+	
+func _on_timer_timeout() -> void:
+	pass
 
 func set_all_processes(enable: bool) -> void:
 	player.set_process(enable)
