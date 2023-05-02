ba31f4bf30fc5c7380d3d247f0744dad71fca6f3
Oscar Mellado <oscar-mellado@uoc.edu>
2023-05-03 00:01:42 +0100

    Add basic PCG platforms

--- Environment/Platforms.gd
+++ Environment/Platforms.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+
 const VELOCITY: float = 2.0
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _process(delta):
 	position.x -= VELOCITY
+	
--- Main.gd
+++ Main.gd
@@ -5,19 +5,22 @@ extends Node2D
 @onready var background: Node2D = $SpaceBackground/SpaceBackground
 @onready var background2: Node2D = $SpaceBackground/SpaceBackground2
 @onready var death_area: Area2D = $DeathArea
+@onready var border_right_screen_area: Area2D = $BorderRightScreenArea
 @onready var ui: Control = $UI
 @onready var ui_info := $UI/PanelContainer/BoxContainer/VBoxContainer/HBoxContainer/Info
 @onready var timer := $Timer
 @onready var retry_btn := $UI/PanelContainer/BoxContainer/VBoxContainer/HBoxContainer2/Button
 
+
 func _ready():
 	set_all_processes(false)
 	death_area.body_exited.connect(_on_exited_death_area)
+	border_right_screen_area.area_entered.connect(_on_entered_screen_area)
 	timer.timeout.connect(_on_timer_timeout)
 	retry_btn.pressed.connect(_on_retry_btn_pressed)
 	timer.start()
 
-func _process(delta):
+func _process(_delta):
 	pass
 	
 func start_game() -> void:
@@ -32,6 +35,13 @@ func finish_game() -> void:
 	
 func _on_exited_death_area(body: Node) -> void:
 	finish_game()
+	
+func _on_entered_screen_area(body: Node) -> void:
+	var last_platform = body.get_parent()
+	var new_platform = preload("res://Environment/TriplePlatform.tscn").instantiate()
+	new_platform.position = last_platform.get_position() + Vector2(500, 0)
+	platforms.add_child(new_platform)
+	
 	
 func _on_timer_timeout() -> void:
 	start_game()
